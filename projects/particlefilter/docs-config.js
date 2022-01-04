1f7d30b7acc00a6be044689043ae080d16bd1d55
whomihirpatel <mspatel1298@gmail.com>
2022-01-04 00:59:28 -0600

    init particle-filter-localization docs

---
+++ projects/particlefilter/docs-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    product: "Particle Filter",
+    pathPrefix: "",
+    productLogoPathD: "",
+    productIconKey: "",
+    contentRepo: "mihyr/particle-filter-localization",
+    contentRepoFolder: "particle-filter-localization",
+    externalLinks: [
+      {
+        title: "Homepage",
+        url: "https://mihr.io/"
+      },
+      {
+        title: "Projects",
+        url: "https://projects.mihr.io/"
+      },
+    ],
+    search: {
+      indexName: "pegasus_drone",
+      apiKey: "f23af949ddb593a485da54c40dcfd03b",
+      algoliaOptions: {
+        facetFilters:  '["project:Particle Filter Localization"]'
+    }
+    },
+    siteMetadata: {
+      title: "Particle Filter Localization",
+      description: "Documentation for Mihir's Projects",
+      author: "@mihyr",
+      url: "http://mihr.io",
+      image: "https://github.com/mihyr.png",
+    }
+  }
